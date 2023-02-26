28cff023cbbec9777b7951d4b09aa967da764ed9
Harshit Raj <harshitr20@iitk.ac.in>
2023-02-26 18:01:56 +0530

    refactor

--- src/parse.py
+++ src/parse.py
@@ -20,21 +20,26 @@ import sys
 
 
 def p_program(p):
-    """program : package_decl imports_decl class_interface_decls"""
-    p[0] = p[1] + p[2] + p[3]
+    """program : OrdinaryCompilationUnit"""
+    p[0] = p[1]
 
 
-def p_package_decl(p):
-    """package_decl : PACKAGE IDENTIFIER iden_dot SEMICOLON
+def p_OrdinaryCompilationUnit(p):
+    """OrdinaryCompilationUnit : BetaPackageDeclaration AlphaImportDeclaration"""# AlphaTopLevelClassOrInterfaceDeclaration"""
+    p[0] = p[1] + p[2] #+ p[3]
+
+
+def p_BetaPackageDeclaration(p):
+    """BetaPackageDeclaration : PACKAGE IDENTIFIER AlphaDotIdentifier SEMICOLON
     | empty"""
     if p[1] == "package":
-        p[0] = "package " + p[2] + ";\r"
+        p[0] = "package " + p[2] + p[3] + ";\r"
     else:
         p[0] = ""
 
 
-def p_iden_dot(p):
-    """iden_dot : DOT IDENTIFIER iden_dot
+def p_AlphaDotIdentifier(p):
+    """AlphaDotIdentifier : DOT IDENTIFIER AlphaDotIdentifier
     | empty"""
     if p[1] == ".":
         p[0] = "." + p[2] + p[3]
@@ -42,8 +47,8 @@ def p_iden_dot(p):
         p[0] = ""
 
 
-def p_imports_decl(p):
-    """imports_decl : IMPORT import_static IDENTIFIER iden_dot import_star SEMICOLON imports_decl
+def p_AlphaImportDeclaration(p):
+    """AlphaImportDeclaration : IMPORT BetaImportStatic IDENTIFIER AlphaDotIdentifier BetaImportStar SEMICOLON AlphaImportDeclaration
     | empty"""
     if p[1] == "import":
         p[0] = "import " + str(p[2]) + p[3] + str(p[4]) + str(p[5]) + ";\r" + str(p[7])
@@ -51,8 +56,8 @@ def p_imports_decl(p):
         p[0] = ""
 
 
-def p_import_static(p):
-    """import_static : STATIC
+def p_BetaImportStatic(p):
+    """BetaImportStatic : STATIC
     | empty"""
     if p[1] == "static":
         p[0] = "static "
@@ -60,8 +65,8 @@ def p_import_static(p):
         p[0] = ""
 
 
-def p_import_star(p):
-    """import_star : DOTSTAR
+def p_BetaImportStar(p):
+    """BetaImportStar : DOTSTAR
     | empty"""
     if p[1] == ".*":
         p[0] = ".*"
@@ -69,44 +74,38 @@ def p_import_star(p):
         p[0] = ""
 
 
-def p_class_interface_decls(p):
-    """class_interface_decls : class_decl class_interface_decls
-        | interface_decl class_interface_decls
-        | SEMICOLON
-        | empty"""
-    if p[1] == ";":
-        p[0] = ";\r"
-    elif p[1] != "":
-        p[0] = str(p[1]) + str(p[2])
-    else:
-        p[0] = ""
+
 
 ############
 # class_decl := CLASS IDENTIFIER class_body
 ############
 
-def p_class_decl(p):
-    """class_decl : class_modifiers CLASS IDENTIFIER class_body"""
-    p[0] = str(p[1]) + "class " + p[3] + str(p[4])
 
-def p_class_modifiers(p):
-    """class_modifiers : class_modifier class_modifiers
-        | empty"""
-    if p[1] != "":
-        p[0] = str(p[1]) + str(p[2])
-    else:
-        p[0] = ""
+# def p_class_decl(p):
+#     """class_decl : class_modifiers CLASS IDENTIFIER type_params class_body"""
+#     p[0] = str(p[1]) + "class " + p[3] + str(p[4])
 
-def p_class_modifier(p):
-    """class_modifier : PUBLIC
-        | PROTECTED
-        | PRIVATE
-        | ABSTRACT
-        | STATIC
-        | FINAL
-        | SEALED
-        | STRICTFP"""
-    p[0] = p[1] + " "
+
+# def p_class_modifiers(p):
+#     """class_modifiers : class_modifier class_modifiers
+#     | empty"""
+#     if p[1] != "":
+#         p[0] = str(p[1]) + str(p[2])
+#     else:
+#         p[0] = ""
+
+
+# def p_class_modifier(p):
+#     """class_modifier : PUBLIC
+#     | PROTECTED
+#     | PRIVATE
+#     | ABSTRACT
+#     | STATIC
+#     | FINAL
+#     | SEALED
+#     | STRICTFP"""
+#     p[0] = p[1] + " "
+
 
 def p_empty(p):
     "empty :"
