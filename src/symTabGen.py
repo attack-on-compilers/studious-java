a90d811218fa5c986a952011213d22f242784061
Kumar Arpit <arpit20@iitk.ac.in>
2023-03-12 16:48:46 +0530

    Current plan

--- src/symTabGen.py
+++ src/symTabGen.py
@@ -1,3 +1,13 @@
+from symbol_table import *
+
+global_symbol_table = SymbolTable()
+current_symbol_table = global_symbol_table
+
 def generate_symbol_table(tree):
     """Generate a symbol table from the given Parse Tree."""
-    pass
+    traverse_tree(tree)
+    return
+
+def traverse_tree(tree):
+    """We perform a depth first traversal of the tree"""
+    
