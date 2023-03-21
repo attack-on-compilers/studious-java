66ff0d0819bfb77522724c38a98f61e6edb0e040
akshanagrawal <akshancs2020@gmail.com>
2023-03-21 14:37:49 +0530

    test case for array check added

---
+++ tests/test_11.java
@@ -0,0 +1,9 @@
+private class TestIfElse {
+    int[] intArray = new int[20];
+    intArray[0] = 10;
+    int[] intArray2 = new int[]{ 1,2,3,4,5,6,7,8,9,10};
+    int[][] arr = new int[10][20];
+    arr[0][0] = 1;
+    int[][] arr2 = { { 1, 2 }, { 3, 4 } };
+
+}
