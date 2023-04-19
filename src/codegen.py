f4a65d2b44a9ade79cee4f650ef3bac6e2f984c7
Harshit Raj <harshitr20@iitk.ac.in>
2023-04-19 22:58:01 +0530

    fmt

--- src/codegen.py
+++ src/codegen.py
@@ -6,26 +6,22 @@ from tac import *
 import string
 import secrets
 
+
 class Register:
     # these registers mapped to None and 0 initially (for no initial stored values and start time)
-    regs = dict.fromkeys(['%rbx', '%r10', '%r11', '%r12', '%r13', '%r14'], [None, 0])
-    heap_reg = dict.fromkeys(['%r15'], [None, 0])
+    regs = dict.fromkeys(["%rbx", "%r10", "%r11", "%r12", "%r13", "%r14"], [None, 0])
+    heap_reg = dict.fromkeys(["%r15"], [None, 0])
 
-    #registers for arguments
-    argument_registers = dict(
-        zip(
-            map(str, range(6)),
-            ['%rdi', '%rsi', '%rdx', '%rcx', '%r8', '%r9']
-        )
-    )
+    # registers for arguments
+    argument_registers = dict(zip(map(str, range(6)), ["%rdi", "%rsi", "%rdx", "%rcx", "%r8", "%r9"]))
 
     RIP_reg = None
     RBP_reg = None
     count = 0
     locations = dict()
 
-    super_registers = ['%rax', '%rbx', '%rcx', '%rdx', '%r8', '%r9', '%r10', '%r11', '%r12', '%r13', '%r14', '%r15']
-    sub_registers = ['%al', '%bl', '%cl', '%dl', '%r8b', '%r9b', '%r10b', '%r11b', '%r12b', '%r13b', '%r14b', '%r15b']
+    super_registers = ["%rax", "%rbx", "%rcx", "%rdx", "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", "%r14", "%r15"]
+    sub_registers = ["%al", "%bl", "%cl", "%dl", "%r8b", "%r9b", "%r10b", "%r11b", "%r12b", "%r13b", "%r14b", "%r15b"]
 
     last_byte_map = dict(zip(super_registers, sub_registers))
 
@@ -36,15 +32,15 @@ class Register:
         ## use the lru policy
         # get the register with the least recent use
         # return the register
-        setmin = 1e10+5
+        setmin = 1e10 + 5
         for key in self.regs:
             if self.regs[key][1] < setmin:
                 setmin = self.regs[key][1]
-                lru_reg = key 
+                lru_reg = key
         return lru_reg
-       
+
     def write_back(self, arr=None, flush=True):
-        #write back the value to memory
+        # write back the value to memory
         instructions = []
         if flush:
             instructions.append("#" + str(self.regs))
@@ -52,67 +48,63 @@ class Register:
             if arr is not None and r not in arr:
                 continue
             if v[0]:
-                loc = self.locations.get(v[0], [None, ''])[1]
-                if loc != 'Tempo':
+                loc = self.locations.get(v[0], [None, ""])[1]
+                if loc != "Tempo":
                     instructions.append(f"\tmov {r}, {loc}")
                 if flush:
                     self.locations.pop(v[0], None)
                     self.regs[r] = [None, 0]
         return instructions
-    
 
     def get_register(self, v=None):
-
-        v = ''.join(secrets.choice(string.ascii_lowercase) for _ in range(8)) if v is None else v
+        v = "".join(secrets.choice(string.ascii_lowercase) for _ in range(8)) if v is None else v
         instructions = []
         if v in self.locations and self.locations[v][0] is not None:
             reg = self.locations[v][0]
-            #self.regs[reg][1] = self.count
+            # self.regs[reg][1] = self.count
             self.count = self.count + 1
             return reg, []
-        
+
         self.count = self.count + 1
         reg = self.lru_policy()
         # instructions = self.write_back([reg], True)
         self.regs[reg] = v, self.count
 
         if v not in self.locations:
-            self.locations[v] = [reg, 'Tempo']
+            self.locations[v] = [reg, "Tempo"]
         else:
             self.locations[v][0] = reg
             instructions.append(f"  mov {self.locations[v][1]}, {reg}")
         return reg, instructions
 
 
-        
-
-
 class ASM:
     def tac_to_x86_mapping(self, tac):
         instructions = []
         reg = Register()
-        tac = [['test_13:'],
-['BeginFunction', 'test_13_main:'],
-['PopFromStack', 'test_13_main_args'],
-['stackpoint++', 8],
-['=', '10', 'test_13_main_a'],
-['stackpoint++', 8],
-['=', '5', 'test_13_main_b'],
-['stackpoint++', 8],
-['-', 'test_13_main_a', 'test_13_main_b', '__t_3'],
-['+', '__t_3', '5', '__t_2'],
-['-', '__t_2', '7', '__t_1'],
-['=', '__t_1', 'test_13_main_c'],
-['stackpoint--', 40],
-['stackpoint--', 16],
-['myClass:'],
-['stackpoint++', 8],
-['stackpoint++', 8],
-['stackpoint++', 8],
-['stackpoint--', 56]]
+        tac = [
+            ["test_13:"],
+            ["BeginFunction", "test_13_main:"],
+            ["PopFromStack", "test_13_main_args"],
+            ["stackpoint++", 8],
+            ["=", "10", "test_13_main_a"],
+            ["stackpoint++", 8],
+            ["=", "5", "test_13_main_b"],
+            ["stackpoint++", 8],
+            ["-", "test_13_main_a", "test_13_main_b", "__t_3"],
+            ["+", "__t_3", "5", "__t_2"],
+            ["-", "__t_2", "7", "__t_1"],
+            ["=", "__t_1", "test_13_main_c"],
+            ["stackpoint--", 40],
+            ["stackpoint--", 16],
+            ["myClass:"],
+            ["stackpoint++", 8],
+            ["stackpoint++", 8],
+            ["stackpoint++", 8],
+            ["stackpoint--", 56],
+        ]
         # Loop through each TAC instruction
         for t in tac:
-
             if len(t) == 4:
                 op, arg1, arg2, res = t[0], t[1], t[2], t[3]
 
@@ -149,7 +141,6 @@ class ASM:
                     instructions.append(f"  mov {reg3}, {reg1}")
 
                 elif op == "*":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -166,26 +157,6 @@ class ASM:
                     instructions.append(f"  mov {reg3}, {reg1}")
 
                 elif op == "/":
-
-                    # Load arg1 into a register
-                    reg1, load1 = reg.get_register(arg1)
-                    instructions.extend(load1)
-
-                    # Load arg2 into a register
-                    reg2, load2 = reg.get_register(arg2)
-                    instructions.extend(load2)
-
-                    reg3, load3 = reg.get_register(res)
-                    instructions.extend(load3)
-
-                    # Divide the values and store the result in res
-                    instructions.append(f"  mov {reg3}, {reg1}")
-                    instructions.append(f"  cqo")
-                    instructions.append(f"  idiv {reg2}")   
-                    #check completeness one more statement may be needded
-
-                elif op == "%":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -201,26 +172,42 @@ class ASM:
                     instructions.append(f"  mov {reg3}, {reg1}")
                     instructions.append(f"  cqo")
                     instructions.append(f"  idiv {reg2}")
-                    #check completeness one more statement may be needded   
+                    # check completeness one more statement may be needded
+
+                elif op == "%":
+                    # Load arg1 into a register
+                    reg1, load1 = reg.get_register(arg1)
+                    instructions.extend(load1)
+
+                    # Load arg2 into a register
+                    reg2, load2 = reg.get_register(arg2)
+                    instructions.extend(load2)
+
+                    reg3, load3 = reg.get_register(res)
+                    instructions.extend(load3)
+
+                    # Divide the values and store the result in res
+                    instructions.append(f"  mov {reg3}, {reg1}")
+                    instructions.append(f"  cqo")
+                    instructions.append(f"  idiv {reg2}")
+                    # check completeness one more statement may be needded
 
                 elif op == ">":
-                        
-                        # Load arg1 into a register
-                        reg1, load1 = reg.get_register(arg1)
-                        instructions.extend(load1)
-    
-                        # Load arg2 into a register
-                        reg2, load2 = reg.get_register(arg2)
-                        instructions.extend(load2)
-    
-                        reg3, load3 = reg.get_register(res)
-                        instructions.extend(load3)
-    
-                        instructions.append(f"  cmp {reg1}, {reg2}")
-                        instructions.append(f"  setg {reg3}")   
+                    # Load arg1 into a register
+                    reg1, load1 = reg.get_register(arg1)
+                    instructions.extend(load1)
+
+                    # Load arg2 into a register
+                    reg2, load2 = reg.get_register(arg2)
+                    instructions.extend(load2)
+
+                    reg3, load3 = reg.get_register(res)
+                    instructions.extend(load3)
+
+                    instructions.append(f"  cmp {reg1}, {reg2}")
+                    instructions.append(f"  setg {reg3}")
 
                 elif op == "<":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -236,7 +223,6 @@ class ASM:
                     instructions.append(f"  setl {reg3}")
 
                 elif op == ">=":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -252,7 +238,6 @@ class ASM:
                     instructions.append(f"  setge {reg3}")
 
                 elif op == "<=":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -268,7 +253,6 @@ class ASM:
                     instructions.append(f"  setle {reg3}")
 
                 elif op == "==":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
@@ -284,114 +268,98 @@ class ASM:
                     instructions.append(f"  sete {reg3}")
 
                 elif op == "!=":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  cmp {reg1}, {reg2}")
-                    instructions.append(f"  setne {reg3}")    
+                    instructions.append(f"  setne {reg3}")
 
                 elif op == "&&":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  and {reg1}, {reg2}")
                     instructions.append(f"  mov {reg3}, {reg1}")
 
                 elif op == "||":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  or {reg1}, {reg2}")
                     instructions.append(f"  mov {reg3}, {reg1}")
 
                 elif op == "^":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  xor {reg1}, {reg2}")
                     instructions.append(f"  mov {reg3}, {reg1}")
 
                 elif op == ">>":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  mov {reg3}, {reg1}")
                     instructions.append(f"  shr {reg3}, {reg2}")
 
                 elif op == "<<":
-
                     # Load arg1 into a register
                     reg1, load1 = reg.get_register(arg1)
                     instructions.extend(load1)
-    
+
                     # Load arg2 into a register
                     reg2, load2 = reg.get_register(arg2)
                     instructions.extend(load2)
-    
+
                     reg3, load3 = reg.get_register(res)
                     instructions.extend(load3)
-    
+
                     instructions.append(f"  mov {reg3}, {reg1}")
                     instructions.append(f"  shl {reg3}, {reg2}")
 
-
-
         pprint(instructions)
-
 
 
 if __name__ == "__main__":
     asm = ASM()
     asm.tac_to_x86_mapping(None)
-
-    
-
-
-
-
-    
