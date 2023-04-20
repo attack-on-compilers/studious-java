b71042c56fc7b4149da8714ab0a7f6bc967e7b62
Kumar Arpit <arpit20@iitk.ac.in>
2023-04-20 18:03:11 +0530

    testcase

--- tests/test_15.java
+++ tests/test_15.java
@@ -1,23 +1,30 @@
 public class test_15 {
 
     static int fun(int x){
-        if (x==1)
-            fun(x+2);
-        System.out.println("fun"+x);
+        // if (x==1)
+        //     fun(x+2);
+        System.out.println("fun "+x);
         return (x+1);
     }
     public static void main(String[] args) {
-        // long a =100;
-        // int b = 5;
-        // long c = a*b;
-        // long d = a+c;
-        // long e = a+b+c+d;
-        // long f = a+b+c+d+e;
-        // long g = a*b*c*d*e*f*a*b*c*d*e*f;
-        int h = fun(fun(1));
+        long a =100;
+        long b = 5;
+        System.out.println("a="+a+" b="+b);
+        long c = a*b;
+        long d = a+c;
+        long e = a+b+c+d;
+        long f = a+b+c+d+e;
+        long g = a*b*c*d*e*f*a*b*c*d*e*f;
+        // long h =100;                         // Uncommenting will make it work
+        // long x = fun(0);
+        // long h = fun(1);
+        int h1 =fun(fun(fun(1)));
+        int h2 =fun(55);
         // System.out.println(h);
+        // System.out.println(h1);
+        // System.out.println(h2);
         // int i = 999;
-        // System.out.println("a="+a+" b="+b+" c="+c+" d="+d+" e="+e +" f="+f+" g="+g+" h="+h);
+        System.out.println("a="+a+" b="+b+" c="+c+" d="+d+" e="+e +" f="+f+" g="+g);
         // System.out.println("apple "+5+"apple "+a);
         
         // return;
