284433cd41bab2458d7dd445acd8dfc868b20846
Kumar Arpit <arpit20@iitk.ac.in>
2023-04-20 16:59:40 +0530

    Some bug fixes

--- tests/test_15.java
+++ tests/test_15.java
@@ -1,17 +1,25 @@
 public class test_15 {
 
-    static int fun(){return 1;}
+    static int fun(int x){
+        if (x==1)
+            fun(x+2);
+        System.out.println("fun"+x);
+        return (x+1);
+    }
     public static void main(String[] args) {
-        long a =100;
+        // long a =100;
         // int b = 5;
         // long c = a*b;
         // long d = a+c;
         // long e = a+b+c+d;
-        // int c = fun();
-        System.out.println("appled");
-        System.out.println("apple "+5+"apple "+a);
         // long f = a+b+c+d+e;
         // long g = a*b*c*d*e*f*a*b*c*d*e*f;
+        int h = fun(fun(1));
+        // System.out.println(h);
+        // int i = 999;
+        // System.out.println("a="+a+" b="+b+" c="+c+" d="+d+" e="+e +" f="+f+" g="+g+" h="+h);
+        // System.out.println("apple "+5+"apple "+a);
+        
         // return;
     }
 }
